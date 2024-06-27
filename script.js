09a4eb6931c37fd9b8ea5b36cdb606145b32a616
AbdallahAwaysheh <awaysheh6930@gmail.com>
2024-06-27 09:56:22 +0300

    update

---
+++ script.js
@@ -0,0 +1,51 @@
+
+// Task 1
+// let header = document.getElementById("header");
+// header.innerHTML="Welcome to DOM Manipulation!";
+// ---------------------------------------------------
+
+
+// Task 2
+// let items = document.getElementsByClassName("item");
+// for (let i = 0;i<4;i++){
+//     items[i].innerHTML="Class Item";
+// }
+// ---------------------------------------------------
+
+
+// Task 3
+// let paragraph = document.getElementsByTagName("p");
+// for (let i = 0;i<4;i++){
+//     paragraph[i].innerHTML="paragraph";
+// }
+// ---------------------------------------------------
+
+
+
+// Task 4
+// let first = document.getElementsByClassName("highlight")[0];
+// first.innerHTML="First Highlighted Item";
+
+
+// Task 5
+// for (let i = 0;i<5;i++){
+//     document.querySelectorAll("li")[i].innerText="List Item";
+
+// }
+
+
+
+// Task 6
+// let foter = document.querySelector("footer");
+// foter.innerText="Footer Updated"; 
+
+// task 7
+
+// document.querySelectorAll('.box')[0].innerHTML="Box Item";
+
+
+
+
+
+
+
